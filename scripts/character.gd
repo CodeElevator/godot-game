986b23dde39b5e856ce0549f44bab68a515f4567
NomCustom <codeelevator131@gmail.com>
2023-03-05 20:29:48 +0200

    Added moving script

--- scripts/character.gd
+++ scripts/character.gd
@@ -6,6 +6,6 @@ func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 
-func _physics_process(_delta):
+func _physics_process(_delta_):
 	get_input()
 	move_and_slide()
